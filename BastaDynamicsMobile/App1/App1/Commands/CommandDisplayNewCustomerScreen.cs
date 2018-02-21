84d787ccd53d21e0c32dbd4c68404197d579da7a
AD-BAYER-CNB\IMZAN <andriy.zhurba@bayer.com>
2018-02-21 17:51:32 +0100

    Xamarin App

---
+++ BastaDynamicsMobile/App1/App1/Commands/CommandDisplayNewCustomerScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace BastaDynamicsShared.Commands
+{
+    public class CommandDisplayNewCustomerDialog : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
