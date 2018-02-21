168e44ff3b7db7d56d9751f742d5bbe006374594
Marc Lucksch <marc_lucksch@hotmail.com>
2018-02-21 18:05:49 +0100

    Getter

--- DataBackend/BASTA-dynamics-Userlib-db/Class1.cs
+++ DataBackend/BASTA-dynamics-Userlib-db/Class1.cs
@@ -5,6 +5,9 @@ using System.Net;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents.Linq;
+using System.Linq.Expressions;
 
 namespace BASTA_dynamics_Userlib_db
 {
@@ -59,11 +62,29 @@ namespace BASTA_dynamics_Userlib_db
             }
         }
 
-        public async CompanyObject[] GetCompany(string filter = "")
+        public async Task<IEnumerable<T>> GetItemsAsync<T>(Expression<Func<T, bool>> predicate, string type)
         {
+            IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(CDName, CollName), "SELECT * FROM c where c.type = \""+type+"\"")
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        public async Task<CompanyObject[]> GetCompany(string filter = "")
+        {
+            var items = (await GetItemsAsync<CompanyObject>(e => filter == "" ? true : e.Name == filter, "company").ConfigureAwait(false));
+            return items.ToArray();
 
         }
-        
+
         public async Task SetCompany(CompanyObject input)
         {
             await CreateDocumentIfNotExists(CDName, CollName, input);
@@ -73,9 +94,10 @@ namespace BASTA_dynamics_Userlib_db
             await DeleteDocument(CDName, CollName, input.Id);
         }
 
-        public async CompanyObject[] GetEmployee(string filter = "")
+        public async Task<EmployeeObject[]> GetEmployee(string filter = "")
         {
-
+            var items = (await GetItemsAsync<EmployeeObject>(e => filter == "" ? true : e.Lastname == filter, "employee").ConfigureAwait(false));
+            return items.ToArray();
         }
 
         public async Task SetEmployee(CompanyObject input)
